ede0b4478f5d7d63efe55f207c56b1fa08738d53
iliapa1 <ilia.nikolaev.parvanov@gmail.com>
2018-07-19 20:03:06 +0300

    Added header which doesn't work

--- cars/carsbg/static/js/main.js
+++ cars/carsbg/static/js/main.js
@@ -1,3 +1,21 @@
+// When the user scrolls the page, execute stickToTop
+window.onscroll = function() {stickToTop()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickToTop() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
 $( function() {
     $.widget( "custom.catcomplete", $.ui.autocomplete, {
       _create: function() {
