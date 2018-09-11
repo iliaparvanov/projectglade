58f77d75118e8aae133c05985aaaa26f4a1cf7e4
Gdemirev01 <gdemirev78@gmail.com>
2018-09-11 23:45:39 +0300

    heroku deploy

--- cars/cars/settings.py
+++ cars/cars/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 #import django_google_maps
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,5 @@ STATICFILES_DIRS = (
 LOGIN_REDIRECT_URL = '/'
 
 LOGIN_URL = '/login'
+
+django_heroku.settings(locals())
